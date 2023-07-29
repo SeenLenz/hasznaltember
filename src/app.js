33ba2deb9849aae599f131fbf0c991c6444639dd
Sipos Milán <Sipos.Milan22@gmail.com>
2023-07-29 14:24:55 +0200

    #update1

---
+++ src/app.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", (e) => {
+  document
+    .querySelectorAll(".project-card")
+    .forEach((e) => e.addEventListener("click", ExpandCard));
+});
+
+function ExpandCard(e) {
+  if (e.target.classList.contains("expandedcard") == false) {
+    e.target.classList.add("expandedcard");
+    document.querySelectorAll("#cardpicture").forEach((e) => {
+      e.classList.add("bigimage");
+    });
+    document.querySelectorAll(".project-card").forEach((e) => {
+      if (e.classList.contains("expandedcard") == false) {
+        e.classList.add("disappearedcard");
+      }
+    });
+  } else if (e.target.classList.contains("expandedcard")) {
+    e.target.classList.remove("expandedcard");
+    document.querySelectorAll(".project-card").forEach((e) => {
+      e.classList.remove("disappearedcard");
+    });
+    document.querySelectorAll("#cardpicture").forEach((e) => {
+      e.classList.remove("bigimage");
+    });
+  }
+}
+
+//"body >*:not(.expandedcard).project-card"
